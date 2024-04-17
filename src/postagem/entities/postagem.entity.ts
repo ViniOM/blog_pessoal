ef51d4f167a6789c01d56c963db62ba1311af7d4
Vinicios <vini.webdeveloper@gmail.com>
2024-04-17 09:06:53 -0300

    validaçoes

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -5,16 +5,19 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 import { IsNotEmpty } from 'class-validator';
+import { Transform, TransformFnParams } from 'class-transformer';
 
 @Entity({ name: 'tb_postagem' })
 export class Postagem {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   @IsNotEmpty()
   @Column({ length: 100, nullable: false })
   titulo: string;
 
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   @Column({ length: 1000, nullable: false })
   @IsNotEmpty()
   texto: string;
