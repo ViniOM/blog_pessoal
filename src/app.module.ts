6ef760c76e786fd2a090398fd31b696bbc59d485
Vinicios <vini.webdeveloper@gmail.com>
2024-04-24 13:30:15 -0300

    Crud do usuario e criptografação da senha

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,9 @@ import { PostagemModule } from './postagem/postagem.module';
 
 import { TemaModule } from './tema/tema.module';
 import { Tema } from './tema/entities/tema.entity';
+import { AuthModule } from './auth/auth.module';
+import { UsuarioModule } from './usuario/usuario.module';
+import { Usuario } from './usuario/entities/usuario.entity';
 
 @Module({
   imports: [
@@ -21,10 +24,12 @@ import { Tema } from './tema/entities/tema.entity';
       database: 'db_blog_pessoal',
       synchronize: true,
       logging: true,
-      entities: [Postagem, Tema],
+      entities: [Postagem, Tema, Usuario],
     }),
     PostagemModule,
     TemaModule,
+    AuthModule,
+    UsuarioModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -8,6 +8,7 @@ import {
 import { IsNotEmpty } from 'class-validator';
 import { Transform, TransformFnParams } from 'class-transformer';
 import { Tema } from 'src/tema/entities/tema.entity';
+import { Usuario } from 'src/usuario/entities/usuario.entity';
 
 @Entity({ name: 'tb_postagens' })
 export class Postagem {
@@ -30,6 +31,10 @@ export class Postagem {
   @ManyToOne(() => Tema, (tema) => tema.postagem, {
     onDelete: 'CASCADE',
   })
-  @IsNotEmpty()
   tema: Tema;
+
+  @ManyToOne(() => Usuario, (usuario) => usuario.postagem, {
+    onDelete: 'CASCADE',
+  })
+  usuario: Usuario;
 }
