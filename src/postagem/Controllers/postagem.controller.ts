ec8427c1059026317806a3fb23a4ac6a947dae8a
ViniOM <vini.webdeveloper@gmail.com>
2024-05-23 20:50:16 -0300

    jwt user fixed

--- src/postagem/Controllers/postagem.controller.ts
+++ src/postagem/Controllers/postagem.controller.ts
@@ -15,49 +15,46 @@ import { PostagemService } from '../services/postagem.service';
 import { Postagem } from '../entities/postagem.entity';
 import { DeleteResult } from 'typeorm';
 import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { jwtConstants } from '../../auth/constants/constants';
 
 @ApiTags('Postagem')
+@UseGuards(JwtAuthGuard)
 @Controller('/postagens')
+@ApiBearerAuth()
 export class PostagemController {
   constructor(private readonly postagemService: PostagemService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Get()
   @HttpCode(HttpStatus.OK)
   findAll(): Promise<Postagem[]> {
     return this.postagemService.findAll();
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get(':id')
   @HttpCode(HttpStatus.OK)
   findByid(@Param('id', ParseIntPipe) id: number): Promise<Postagem> {
     return this.postagemService.findById(id);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get('/titulo/:id')
   @HttpCode(HttpStatus.OK)
   findByTitulo(@Param('id') id: string): Promise<Postagem[]> {
     return this.postagemService.findByTitulo(id);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Post('/criar')
   @HttpCode(HttpStatus.CREATED)
   create(@Body() postagem: Postagem): Promise<Postagem> {
     return this.postagemService.create(postagem);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Put('/atualizar')
   @HttpCode(HttpStatus.CREATED)
   async atualizar(@Body() postagem: Postagem): Promise<Postagem> {
     return this.postagemService.update(postagem);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Delete('/deletar/:id')
   @HttpCode(HttpStatus.OK)
   delete(@Param('id', ParseIntPipe) id: number): Promise<DeleteResult> {
